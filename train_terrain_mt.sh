4ffb3d4d82ce20421af9169ce7d3af2c1a71ef66
zzzihanw <zzzihanw@u8d25bda6752559.ant.amazon.com>
2025-12-30 20:12:20 -0500

    set_train_mt

---
+++ train_terrain_mt.sh
@@ -0,0 +1,12 @@
+python src/holosoma/holosoma/train_agent.py \
+  exp:g1-29dof-wbt-motion-tracking-transformer \
+  --training.num_envs=8192 \
+  --command.setup_terms.motion_command.params.motion_config.num_future_steps=10 \
+  --command.setup_terms.motion_command.params.motion_config.target_pose_type=max-coords-future-rel-with-time \
+  --algo.config.module_dict.actor.layer_config.encoder_num_steps=10 \
+  --algo.config.module_dict.critic.layer_config.encoder_num_steps=10 \
+    terrain:terrain-load-obj \
+  --terrain.terrain-term.obj-file-path src/holosoma_retargeting/demo_data/far_robot/far_robot/stairs.obj \
+  \
+  --command.setup_terms.motion_command.params.motion_config.motion_file src/holosoma_retargeting/converted_res/object_interaction/far_robot_mj.npz \
+   --randomization.setup_terms.push_randomizer_state.params.enabled=False \
