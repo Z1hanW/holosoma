eded04b631ce52be4e1b12c5325c4dbd3074b39b
Ha1O7 <76957722+Ha1O7@users.noreply.github.com>
2026-01-05 16:35:40 -0800

    updated

---
+++ eval_terrain_sim2sim.sh
@@ -0,0 +1,14 @@
+python src/holosoma/holosoma/eval_agent.py \
+  --checkpoint=./model_01200.pt \
+  --training.num_envs=1 \
+  --sim2sim.enabled True \
+  --sim2sim.simulator mujoco \
+  --sim2sim.auto-launch True \
+  terrain:terrain-load-obj \
+  --terrain.terrain-term.spawn.randomize_tiles=False \
+  --terrain.terrain-term.obj-file-path stairs.obj \
+  --command.setup_terms.motion_command.params.motion_config.motion_file far_robot_mj.npz \
+  --command.setup_terms.motion_command.params.motion_config.enable_default_pose_append=False \
+  --command.setup_terms.motion_command.params.motion_config.default_pose_append_duration_s=0 \
+  --command.setup_terms.motion_command.params.motion_config.enable_default_pose_prepend=False \
+  --command.setup_terms.motion_command.params.motion_config.default_pose_prepend_duration_s=0
