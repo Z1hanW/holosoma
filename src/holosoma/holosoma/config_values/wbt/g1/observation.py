4702de95f9fa0b8e9ffb35b27f3247d6b326ccb3
Ha1O7 <76957722+Ha1O7@users.noreply.github.com>
2026-02-12 21:24:45 -0800

    behave

--- src/holosoma/holosoma/config_values/wbt/g1/observation.py
+++ src/holosoma/holosoma/config_values/wbt/g1/observation.py
@@ -53,6 +53,33 @@ actor_obs_motion_tracking = ObsGroupCfg(
     terms=actor_obs_motion_tracking_terms,
 )
 
+actor_obs_w_object_terms = actor_obs_shared.terms.copy()
+actor_obs_w_object_terms.update(
+    {
+        "obj_pos_b": ObsTermCfg(
+            func="holosoma.managers.observation.terms.wbt:obj_pos_b",
+            scale=1.0,
+            noise=0.0,
+        ),
+        "obj_ori_b": ObsTermCfg(
+            func="holosoma.managers.observation.terms.wbt:obj_ori_b",
+            scale=1.0,
+            noise=0.0,
+        ),
+        "obj_lin_vel_b": ObsTermCfg(
+            func="holosoma.managers.observation.terms.wbt:obj_lin_vel_b",
+            scale=1.0,
+            noise=0.0,
+        ),
+    }
+)
+actor_obs_w_object = ObsGroupCfg(
+    concatenate=actor_obs_shared.concatenate,
+    enable_noise=actor_obs_shared.enable_noise,
+    history_length=actor_obs_shared.history_length,
+    terms=actor_obs_w_object_terms,
+)
+
 motion_future_target_poses_group = ObsGroupCfg(
     concatenate=True,
     enable_noise=False,
@@ -186,7 +213,7 @@ g1_29dof_wbt_observation_motion_tracking_split = ObservationManagerCfg(
 
 g1_29dof_wbt_observation_w_object = ObservationManagerCfg(
     groups={
-        "actor_obs": actor_obs_shared,
+        "actor_obs": actor_obs_w_object,
         "critic_obs": ObsGroupCfg(
             concatenate=True,
             enable_noise=False,
