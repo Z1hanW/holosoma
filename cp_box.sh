c805cf948ad8f9b6564c3c0ac03ac8d211e46461
Ubuntu <ubuntu@ip-10-0-55-60.us-east-2.compute.internal>
2026-04-22 22:40:30 +0000

    added

--- cp_box.sh
+++ cp_box.sh
@@ -1,3 +1,4 @@
 mkdir -p data
 rm -rf /home/ubuntu/FAR/holosoma/data/ds_box_data
-rsync -avh /nfs/zzzihanw/ds_box_data data
+mkdir -p data/ds_box_data
+rsync -avh /nfs/zzzihanw/ds_box_data/scale_mix_all data/ds_box_data
