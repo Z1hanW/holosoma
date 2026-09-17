2a5cba0bbc90f64dd04fdab5b8d726ae28802b75
YanjieZe <zeyanjie@sjtu.edu.cn>
2026-09-17 16:25:59 -0700

    Use latest SW checkpoint by default

--- real_drop.sh
+++ real_drop.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$ROOT_DIR"
 
 interface="${HOLOSOMA_REAL_INTERFACE:-eth0}"
-default_model_path="${ROOT_DIR}/_ckps/izz2f2df_model_11000.onnx"
+default_model_path="${ROOT_DIR}/_ckps/swl41n4x_model_15500.onnx"
 model_path="${HOLOSOMA_REAL_MODEL_PATH:-$default_model_path}"
 
 log_dir="${ROOT_DIR}/logs/real_drop_$(date +%Y%m%d_%H%M%S)"
